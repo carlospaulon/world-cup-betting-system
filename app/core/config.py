45673a66ea33fdbc9ddfff4f20e2fda247037490
Carlos <paulonluis222@gmail.com>
2026-06-16 17:29:14 -0300

    feat(core): add application settings management

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    DATABASE_URL: str
+    POSTGRES_PASSWORD: str
+    SECRET_KEY: str = ''
+    ALGORITHM: str = ''
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
+@lru_cache
+def get_settings():
+    return Settings()
