b210ebe285bcc71cb9dd4190522276a229dd2dc4
Carlos <paulonluis222@gmail.com>
2026-07-07 22:27:55 -0300

    feat: add global exception handler

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import JSONResponse
 from .core.config import Settings, get_settings
 from .core.database import get_db
 from sqlalchemy import text
 from sqlalchemy.orm import Session
+from .core.exceptions import AppException
 
 app = FastAPI()
+
+# Separar a camada de exception handler em core e chamar no main o def handler.
+
+@app.exception_handler(AppException)
+def global_exception_handler(request: Request, exc: AppException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "error": exc.__class__.__name__,
+            "message": exc.message,
+        }
+    )
 
 
 @app.get('/info')
