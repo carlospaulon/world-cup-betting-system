240f3f4e1610ad08aeedff36e85e234dd3f55dc1
Carlos <paulonluis222@gmail.com>
2026-07-08 23:14:27 -0300

    feat(schemas): add user field validators with CPF validation

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,11 @@
 import uuid
 from datetime import date, datetime
 from pydantic import BaseModel, EmailStr, Field, field_validator, ConfigDict
+from validate_docbr import CPF
+from app.core.exceptions import UnderageUserException
+from app.core.security import validate_password_strength
+
+cpf_validator = CPF()
 
 class UserCreate(BaseModel):
     nickname: str = Field(min_length=3)
@@ -11,7 +16,33 @@ class UserCreate(BaseModel):
 
     model_config = ConfigDict(from_attributes=True)
 
-# TODO: Onde fica o field validator do CPF, e como fazer a senha strong?
+    @field_validator('cpf')
+    @classmethod
+    def check_cpf(cls, value: str):
+        clean_cpf = "".join(filter(str.isdigit, value)) # Limpando o cpf com caracteres
+
+        if not cpf_validator.validate(clean_cpf):
+            raise ValueError("CPF inválido")
+
+        return clean_cpf
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def check_age(cls, value: date):
+        today = date.today()
+            # ano atual - ano informado     mes e dia atual for menor que mes e dia informado (subtraio para pegar a idade real)
+        age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+        if age < 18:
+            raise UnderageUserException("O usuário deve ser maior de 18 anos", 400)
+        return value
+
+    @field_validator("password")
+    @classmethod
+    def check_password(value: str) -> str:
+        validate_password_strength(value)
+        return value
+
+
 class UserResponse(BaseModel):
     id: uuid.UUID
     nickname: str
